07cc4b78d90cb51eed57a52efdf88b5be7dd5d37
Prateek Kumar <prateek@prateekkumar.in>
2020-10-20 22:48:55 +0530

    remove ohmyzsh installations steps in install.sh
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ set -x
 
 sudo -v
 
-sudo add-apt-repository -y ppa:git-core/ppa
-sudo apt install -y zsh git stow
+# sudo add-apt-repository -y ppa:git-core/ppa
+# sudo apt install -y zsh git stow curl
 
-OH_MY_ZSH_DIR=$HOME/.oh-my-zsh
+# OH_MY_ZSH_DIR=$HOME/.oh-my-zsh
 
-if [ ! -d $OH_MY_ZSH_DIR ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+# if [ ! -d $OH_MY_ZSH_DIR ]; then
+#     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# fi
 
-ZSH_SUGGESTIONS_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-if [ ! -d $ZSH_SUGGESTIONS_DIR ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_SUGGESTIONS_DIR
-fi
+# ZSH_SUGGESTIONS_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# if [ ! -d $ZSH_SUGGESTIONS_DIR ]; then
+#     git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_SUGGESTIONS_DIR
+# fi
 
 OLD_DOTFILES="$HOME/dotfiles_bk_$(date -u +"%Y%m%d%H%M%S")"
 mkdir $OLD_DOTFILES
